4c0506fa35148d3c1fcfbf2c3aaf9e6719b21dea
repl.it user <replituser@example.com>
2021-01-04 19:28:19 +0000

    Finished Project

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -40,8 +40,35 @@ def convert_markdown_to_html(markdown):
     """
     Basic markdown to HTML converter. 
     """    
-    markdowner = Markdown()
-    return markdowner.convert(markdown)
+    # I attempted to do a markdown to html converter, but I gave up in the middle, ignore the following code.
+
+    """
+    # Match h1's (1 #)
+    heading_1_regex = re.compile(r"^#\s*(\w+)", re.MULTILINE)
+    result = re.sub(heading_1_regex, r"<h1>\1</h1>", markdown)
+
+    # Match h2's (2 ##)
+    heading_2_regex = re.compile(r"^##\s*(\w+)", re.MULTILINE)
+    result = re.sub(heading_2_regex, r"<h2>\1</h2>", result)
+
+    # Match Bold Text (**TEXT**)
+    bold_text_regex = re.compile(r"\*\*(.*?)\*\*")
+    result = re.sub(bold_text_regex, r"<b>\1</b>", result)
+
+    # Links
+    link_regex = re.compile(r"\[(.*?)\]\((.*?)\)")
+    result = re.sub(link_regex, r'<a href="\2">\1</a>', result)
+
+    # Unordered Lists
+    ul_list_regex = re.compile(r"^((?:[\*\-].*)+)", re.MULTILINE)
+    result = re.sub(ul_list_regex, r'<ul>\1</ul>', result)
+
+    """
+
+
+
+    result = Markdown().convert(markdown)
+    return result
     
 
 
